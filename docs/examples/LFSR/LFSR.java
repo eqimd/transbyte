791018c22694eb20fa92d541b8c6cbb1d1d41562
Daniil Lyubaev <lyubaevda@gmail.com>
2022-11-05 11:37:16 +0300

    Add LFSR .java and .class files

---
+++ docs/examples/LFSR/LFSR.java
@@ -0,0 +1,25 @@
+// Polynom: P(x) = x^19 + x^18 + x^17 + x^14 + 1
+
+class LFSR {
+
+    public static boolean[] get_lfsr(boolean[] reg) {
+        boolean[] output = new boolean[100];
+        for (int i = 0; i < 100; i++) {
+            output[i] = shiftReg(reg);
+        }
+
+        return output;
+    }
+
+    static boolean shiftReg(boolean[] reg) {
+        boolean x = reg[18];
+        boolean y = reg[18]^reg[17]^reg[16]^reg[13];
+        for (int j = 18; j > 0; --j) {
+            reg[j] = reg[j - 1];
+        }
+
+        reg[0] = y;
+
+        return x;
+    }
+}
