60baa7851b47fe0b514cba545473523d593c44c0
Daniil Lyubaev <lyubaevda@gmail.com>
2023-01-04 16:39:01 +0300

    Add simple CLI

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,31 +1,61 @@
 import bit_scheduler.BitSchedulerImpl
-import constants.Constants.BOOLEAN_BITS
-import constants.Constants.INT_BITS
+import constants.Constants
+import kotlinx.cli.ArgParser
+import kotlinx.cli.ArgType
+import kotlinx.cli.default
+import kotlinx.cli.multiple
+import kotlinx.cli.required
 import org.apache.bcel.classfile.ClassParser
-import parsed_types.ClassSat
-import parsed_types.MethodSat
-import parsed_types.data.Variable
+import org.slf4j.simple.SimpleLogger
+import translator.BytecodeTranslatorImpl
 
 fun main(args: Array<String>) {
-    val classFilePath = "docs/examples/LFSR/LFSR.class"
-    val classParser = ClassParser(classFilePath)
-    val clazz = classParser.parse()
+    val argParser = ArgParser(Constants.PROGRAM_NAME)
+
+    val isDebug by argParser.option(
+        ArgType.Boolean,
+        shortName = "d",
+        fullName = "debug",
+        description = "Turn on debug info"
+    ).default(false)
+
+    val classFilePaths by argParser.option(
+        ArgType.String,
+        shortName = "c",
+        fullName = "classes",
+        description = "All paths to classes for the translator"
+    ).multiple()
+
+    val startClass by argParser.option(
+        ArgType.String,
+        shortName = "s",
+        fullName = "start-class",
+        description = "Class name where to find start method"
+    ).required()
+
+    val methodStartName by argParser.option(
+        ArgType.String,
+        shortName = "m",
+        fullName = "method",
+        description = "Name of the method to start translation with"
+    ).default(Constants.TRANSLATOR_START_FUNCTION_NAME)
+
+    argParser.parse(args)
+
+    if (isDebug) {
+        System.setProperty(SimpleLogger.DEFAULT_LOG_LEVEL_KEY, Constants.LOG_LEVEL_DEBUG)
+    }
+    val classes = Array(classFilePaths.size) { i ->
+        val classParser = ClassParser(classFilePaths[i])
+        classParser.parse()
+    }
 
     val bitScheduler = BitSchedulerImpl()
-    val classSat = ClassSat(clazz, bitScheduler)
-    val arg1 = Variable.BitsArrayWithNumber(bitScheduler.getAndShift(INT_BITS))
-    val arg2 = Variable.BitsArrayWithNumber(bitScheduler.getAndShift(INT_BITS))
+    val translator = BytecodeTranslatorImpl(*classes, bitScheduler = bitScheduler)
 
-//    val f = classSat.getMethodByDescription("sum:(II)I")!!
-//    val retVal = f.parse(arg1, arg2) as MethodSat.MethodParseReturnValue.SystemWithPrimitive
-    val bitArr = Variable.ArrayReference.ArrayPrimitives(100, BOOLEAN_BITS, bitScheduler)
-    val f = classSat.getMethodByDescription("get_lfsr:([Z)[Z")!!
-    val retVal = f.parse(bitArr) as MethodSat.MethodParseReturnValue.SystemOnly
-    for (sys in retVal.system) {
-        println()
-        for (eq in sys) {
-            println(eq)
-        }
+    val circuit = translator.translate(startClass, methodStartName)
+
+    for (eq in circuit.system) {
+        println(eq)
     }
-//    println(retVal.primitive.constant)
 }
